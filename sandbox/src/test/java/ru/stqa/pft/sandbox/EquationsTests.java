e87ce80b055294d346e5b883e5bf3851763a03ac
KirillLapizkiy <kir9lapizkiy@gmail.com>
2020-10-16 14:39:04 +0700

    Подкрутил возможность запускать тесты на разных браузерах

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationsTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationsTests.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class RectangleTests {
+public class EquationsTests {
 
   @Test
   public void test0() {
@@ -22,4 +22,23 @@ public class RectangleTests {
     Equations e = new Equations(1, 5, 6);
     Assert.assertEquals(e.rootNumber(), 2);
   }
+
+  @Test
+  public void testLinear() {
+    Equations e = new Equations(0, 1, 1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+  @Test
+  public void testConstant() {
+    Equations e = new Equations(0, 0, 1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+
+  @Test
+  public void testZero() {
+    Equations e = new Equations(0, 0, 0);
+    Assert.assertEquals(e.rootNumber(), -1);
+  }
+
+
 }
