1f8820b937368e3162f0f555e49d07e7602d5a56
KirillLapizkiy <kir9lapizkiy@gmail.com>
2020-11-13 10:25:24 +0700

    Написано и обеспечение выполнение предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -2,12 +2,16 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactModificationTests extends TestBase {
 
     @Test
     public void testContactModification() {
         app.getNavigationHelper().gotoHomePage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("Test1", "test1", "test1"));
+        }
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData("test_name", "test_surname", null), false);
         app.getContactHelper().submitContactModification();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -10,11 +10,7 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("Test1", "test1", "test1"));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
-    app.logout();
+    app.getGroupHelper().createGroup(new GroupData("Test1", "test1", "test1"));
   }
 
 }
